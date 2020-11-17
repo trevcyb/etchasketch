231a1c6cde280601525eb264cc302244515c5569
Trevon Tewari <trevontewari@gmail.com>
2020-11-17 15:38:30 -0400

    Everything works, need to add colour changes, rainbow and style page

--- etch.js
+++ etch.js
@@ -1,23 +1,47 @@
 const container = document.querySelector("#container");
 let bgcolor = "black";
 
-function makeRows (rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+function makeRows (gridSize) {
+    Area = gridSize*gridSize;
+    container.style.setProperty('--grid-rows', gridSize);
+    container.style.setProperty('--grid-cols', gridSize);
 
-    for(c = 0; c < (rows * cols); c++) {
+    for(c = 0; c < (Area); c++) {
         let cell = document.createElement("div");
-        cell.innerText = c + 1;
         container.appendChild(cell).className = "grid-item";
     };
 
-    let chng = container.querySelectorAll("div");
-    chng.forEach(chng => chng.addEventListener("mouseover", function() {
-        chng.style.backgroundColor = bgcolor;
+    let pixels = container.querySelectorAll("div");
+    pixels.forEach(pixel => pixel.addEventListener("mouseover", function() {
+        pixel.style.backgroundColor = bgcolor;
     }));
 
 }
 
+function clearAll() {
+    let pixels = container.querySelectorAll("div");
+    pixels.forEach(pixel => pixel.style.backgroundColor = '#ffffff')
+}
+
+function resize() {
+    let pixels = container.querySelectorAll("div");
+    pixels.forEach(pixel => pixel.remove());
+    let gridSize = prompt("Please enter a new grid size between 4 and 100: ");
+    makeRows(gridSize);
+}
+
+function colors() {
+    switch(bgcolor) {
+        case 'rainbow':
+            this.style.backgroundColor = `hsl(${Math.random() * 360, 100%, 50%})`;
+        case 'black':
+            this.style.backgroundColor = "black";
+        
+    }
+}
+
+document.getElementById("resize").onclick = resize;
+document.getElementById("clear").onclick = clearAll;
 
 
-makeRows(16, 16);
+makeRows(16);

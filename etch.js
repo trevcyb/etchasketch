0be8d5ef915b1aa6bf0ca9e6f808e465a4ef6c55
Trevon Tewari <trevontewari@gmail.com>
2020-11-20 15:56:18 -0400

    Everything now works fine. Add features later.

--- etch.js
+++ etch.js
@@ -1,7 +1,8 @@
 const container = document.querySelector("#container");
 const chosenColor = document.querySelector("#chosenColor");
+const colorbuttons = document.querySelectorAll(".colorbtn");
 
-let bgcolor = "black";
+let bgcolor = 'black';
 
 function makeRows (gridSize) {
     Area = gridSize*gridSize;
@@ -14,10 +15,8 @@ function makeRows (gridSize) {
     };
 
     let pixels = container.querySelectorAll("div");
-    pixels.forEach(pixel => pixel.addEventListener("mouseover", function() {
-        pixel.style.backgroundColor = bgcolor;
-    }));
-
+    pixels.forEach(pixel => pixel.addEventListener("mouseover", colorGen));
+    
 }
 
 function clearAll() {
@@ -29,12 +28,42 @@ function resize() {
     let pixels = container.querySelectorAll("div");
     pixels.forEach(pixel => pixel.remove());
     let gridSize = prompt("Please enter a new grid size between 4 and 100: ");
+    if (gridSize < 4) {
+        alert("Grid Size too low");
+        gridSize = 4;
+    } else if (gridSize > 100) {
+        alert("Grid Size too high");
+        gridSize = 100;
+    }
     makeRows(gridSize);
 }
 
+function colorGen() {
+    switch (bgcolor) {
+        case 'rainbow':
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100% , 50%)`;
+            break;
+        case 'black':
+            this.style.backgroundColor = 'black';
+            break;
+        default:
+            this.style.backgroundColor = bgcolor;
+            break;
+    }
+}
+
 function selectColors(event) {
-    let pixels = container.querySelectorAll("div");
-    pixels.forEach(pixel => pixel.style.backgroundColor = event.target.value);
+    switch(event.target.dataset.color) {
+        case 'rainbow':
+            bgcolor = 'rainbow';
+            break;
+        case 'black':
+            bgcolor = 'black';
+            break;
+        default:
+            bgcolor = 'black';
+            break;
+    }
 }
 
 function userSelectColors(event) {
@@ -46,6 +75,7 @@ document.getElementById("resize").onclick = resize;
 document.getElementById("clear").onclick = clearAll;
 chosenColor.addEventListener("input", userSelectColors, false);
 chosenColor.addEventListener("change", userSelectColors, false);
+colorbuttons.forEach(colorbutton => colorbutton.addEventListener("click", selectColors));
 
 
 makeRows(16);
